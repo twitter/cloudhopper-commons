711e3af91c18018480564866367fa4909a2e862e
joelauer <joelauer@db02c596-f934-4852-9b2a-e10ddaadd22d>
2009-03-27 20:58:41 +0000

    Added JdbcUtil
    git-svn-id: https://svn.cloudhopper.lan/repos/dev/utility/java/ch-commons-sql/trunk@690 db02c596-f934-4852-9b2a-e10ddaadd22d

---
+++ src/main/java/com/cloudhopper/commons/sql/JdbcUtil.java
@@ -0,0 +1,84 @@
+
+package com.cloudhopper.commons.sql;
+
+// java imports
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import org.apache.log4j.Logger;
+
+/**
+ * Common utilities for woking with JDBC.
+ *
+ * @author joelauer
+ */
+public class JdbcUtil {
+    private static Logger logger = Logger.getLogger(JdbcUtil.class);
+
+    /**
+     * Safely closes resources and logs errors.
+     * @param conn Connection to close
+     * @param stmt Statement to close
+     * @param rs ResultSet to close
+     */
+    public static void close(Connection conn, Statement stmt, ResultSet rs) {
+        close(rs);
+        close(stmt);
+        close(conn);
+    }
+
+    /**
+     * Safely closes resources and logs errors.
+     * @param stmt Statement to close
+     * @param rs ResultSet to close
+     */
+    public static void close(Statement stmt, ResultSet rs) {
+        close(rs);
+        close(stmt);
+    }
+
+    /**
+     * Safely closes resources and logs errors.
+     * @param conn Connection to close
+     */
+    public static void close(Connection conn) {
+        if (conn != null) {
+            try {
+                conn.close();
+            } catch (SQLException ex) {
+                logger.error(ex);
+            }
+        }
+    }
+
+    /**
+     * Safely closes resources and logs errors.
+     * @param stmt Statement to close
+     */
+    public static void close(Statement stmt) {
+        if (stmt != null) {
+            try {
+                stmt.close();
+            } catch (SQLException ex) {
+                logger.error(ex);
+            }
+        }
+    }
+
+    /**
+     * Safely closes resources and logs errors.
+     * @param rs ResultSet to close
+     */
+    public static void close(ResultSet rs) {
+        if (rs != null) {
+            try {
+                rs.close();
+            } catch (SQLException ex) {
+                logger.error(ex);
+            }
+        }
+    }
+}
+
