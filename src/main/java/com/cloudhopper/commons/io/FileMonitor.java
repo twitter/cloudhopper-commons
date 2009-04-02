97ae4568af24d471282cc575d3442229f6c17ca2
joelauer <joelauer@db02c596-f934-4852-9b2a-e10ddaadd22d>
2009-04-02 04:18:48 +0000

    Moved to com.cloudhopper package
    git-svn-id: https://svn.cloudhopper.lan/repos/dev/utility/java/ch-commons-io/trunk@724 db02c596-f934-4852-9b2a-e10ddaadd22d

--- src/main/java/com/cloudhopper/commons/io/FileMonitor.java
+++ src/main/java/com/cloudhopper/commons/io/FileMonitor.java
@@ -14,7 +14,7 @@
  * Software Foundation, Inc., 59 Temple Place - Suite 330, Boston, 
  * MA  02111-1307, USA.
  */
-package net.cloudhopper.commons.io;
+package com.cloudhopper.commons.io;
 
 import java.util.*;
 import java.io.File;
