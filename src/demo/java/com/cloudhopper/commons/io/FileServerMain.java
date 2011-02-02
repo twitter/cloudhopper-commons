fda45f513c2c2cc408da91222b243c828d6f7d94
garth <garth@db02c596-f934-4852-9b2a-e10ddaadd22d>
2011-02-02 00:37:15 +0000

    testing changes for FileStore
    git-svn-id: https://svn.cloudhopper.lan/repos/dev/utility/java/ch-commons-io/trunk@3465 db02c596-f934-4852-9b2a-e10ddaadd22d

--- src/demo/java/com/cloudhopper/commons/io/FileServerMain.java
+++ src/demo/java/com/cloudhopper/commons/io/FileServerMain.java
@@ -8,6 +8,10 @@ import java.io.RandomAccessFile;
 import java.net.URL;
 import java.net.URLConnection;
 import java.nio.channels.FileChannel;
+import java.util.Iterator;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 import com.cloudhopper.commons.io.FileStore;
 import com.cloudhopper.commons.io.Id;
@@ -16,6 +20,7 @@ import com.cloudhopper.commons.io.SimpleNIOFileStore;
 import com.cloudhopper.commons.io.UUIDIdGenerator;
 import com.cloudhopper.commons.io.netty.HttpStaticFileServer;
 
+import org.apache.commons.io.FileUtils;
 import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.Logger;
 
@@ -28,27 +33,60 @@ public class FileServerMain
 
     public static void main(String[] argv)
     {
-	BasicConfigurator.configure();
-	Logger log = Logger.getLogger(FileServerMain.class);
+	loadFilesFromDir(argv[0], Integer.parseInt(argv[1]));
 
-	try {
-	    IdGenerator idGen = new UUIDIdGenerator();
-	    FileStore store = new SimpleNIOFileStore(idGen, "/tmp/fileStore/");
+// 	BasicConfigurator.configure();
+// 	Logger log = Logger.getLogger(FileServerMain.class);
+
+// 	try {
+// 	    IdGenerator idGen = new UUIDIdGenerator();
+// 	    FileStore store = new SimpleNIOFileStore(idGen, "/tmp/fileStore/");
 	    
-	    HttpStaticFileServer server = new HttpStaticFileServer(store, 8080);
-	    server.start();
+// 	    HttpStaticFileServer server = new HttpStaticFileServer(store, 8080);
+// 	    server.start();
 	    
-	    File f = new File("test.png");
-	    RandomAccessFile randomAccessFile = new RandomAccessFile(f, "r");
-	    FileChannel fileChannel = randomAccessFile.getChannel();
+// 	    File f = new File("test.png");
+// 	    RandomAccessFile randomAccessFile = new RandomAccessFile(f, "r");
+// 	    FileChannel fileChannel = randomAccessFile.getChannel();
 	    
-	    Id fileId = store.write(fileChannel);
+// 	    Id fileId = store.write(fileChannel);
 	    
-	    saveFileFromUrl(new URL("http://"+fileId.getHost()+":8080/"+fileId.getName()), "test2.png");
+// 	    saveFileFromUrl(new URL("http://"+fileId.getHost()+":8080/"+fileId.getName()), "test2.png");
 	    
-	    //server.stop();
-	} catch (Exception e) {
-	    log.error(e);
+// 	    //server.stop();
+// 	} catch (Exception e) {
+// 	    log.error(e);
+// 	}
+    }
+
+    public static void loadFilesFromDir(String dir, int threads)
+    {
+	ThreadPoolExecutor ex =  new ThreadPoolExecutor(threads, threads, 5000l, TimeUnit.MILLISECONDS, new LinkedBlockingQueue<Runnable>());
+	ex.prestartAllCoreThreads();
+
+	IdGenerator idGen = new UUIDIdGenerator();
+	final FileStore store = new SimpleNIOFileStore(idGen, "/tmp/fileStore/");
+
+	final long start = System.currentTimeMillis();
+	int count = 0;
+
+	Iterator<File> it = FileUtils.iterateFiles(new File(dir), null, true);
+	while (it.hasNext()) {
+	    final File f = it.next();
+	    final int num = count++;
+	    Runnable job = new Runnable() {
+		    public void run() {
+			try {
+			    RandomAccessFile randomAccessFile = new RandomAccessFile(f, "r");
+			    FileChannel fileChannel = randomAccessFile.getChannel();
+			    Id id = store.write(fileChannel);
+			    System.out.println("("+num+") Stored "+f.getPath()+" as "+id.getName()+" after "+(System.currentTimeMillis()-start)+"ms");
+			} catch (Exception e) {
+			    e.printStackTrace();
+			}
+		    }
+		};
+	    ex.execute(job);
 	}
     }
 
