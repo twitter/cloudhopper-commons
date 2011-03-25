f0a011fcb6738e3a6b9cd06316eeb04ec7325d66
garth <garth@db02c596-f934-4852-9b2a-e10ddaadd22d>
2011-03-25 00:15:16 +0000

    added original filename and mimetype to filestore ID
    git-svn-id: https://svn.cloudhopper.lan/repos/dev/utility/java/ch-commons-io/trunk@3719 db02c596-f934-4852-9b2a-e10ddaadd22d

--- src/main/java/com/cloudhopper/commons/io/Id.java
+++ src/main/java/com/cloudhopper/commons/io/Id.java
@@ -1,5 +1,8 @@
 package com.cloudhopper.commons.io;
 
+import javax.activation.MimeType;
+import javax.activation.MimeTypeParseException;
+
 /**
  * An ID representing a file
  * @author garth
@@ -15,9 +18,27 @@ public class Id
 
     private String host;
     private String name;
+    private String originalName;
+    private MimeType mimeType;
 
     public String getHost() { return this.host; }
     public String getName() { return this.name; }
+    public String getOriginalName() { return this.originalName; }
+    public void setOriginalName(String s) { this.originalName = s; }
+    public MimeType getMimeType() { return this.mimeType; }
+    public void setMimeType(MimeType m) { this.mimeType = m; }
+    public void setMimeType(String s) throws MimeTypeParseException { this.mimeType = new MimeType(s); }
 
-    public String toString() { return host+":"+name; }
+    public String toString()
+    {
+	StringBuilder o = new StringBuilder();
+	o.append(host);
+	o.append(":");
+	o.append(name);
+	o.append(" (");
+	o.append(originalName);
+	if (mimeType != null) o.append(":").append(mimeType.getBaseType());
+	o.append(")");
+	return o.toString();
+    }
 }
