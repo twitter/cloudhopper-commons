60b1d8084f1c75de9b5cbfa64052caba0b136d24
garth <garth@twitter.com>
2013-11-14 08:56:08 -0800

    prepare for oss

--- src/demo/java/com/cloudhopper/sxmp/SubmitMain.java
+++ src/demo/java/com/cloudhopper/sxmp/SubmitMain.java
@@ -1,17 +1,3 @@
-/**
- * Copyright (C) 2011 Twitter, Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this
- * file except in compliance with the License. You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software distributed
- * under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
- * CONDITIONS OF ANY KIND, either express or implied. See the License for the
- * specific language governing permissions and limitations under the License.
- */
-
 package com.cloudhopper.sxmp;
 
 /*
@@ -36,9 +22,6 @@ package com.cloudhopper.sxmp;
 
 import com.cloudhopper.commons.util.DecimalUtil;
 import org.apache.http.HttpVersion;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.ResponseHandler;
 import org.apache.http.client.methods.HttpPost;
@@ -46,6 +29,8 @@ import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.BasicResponseHandler;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.params.CoreProtocolPNames;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  *
