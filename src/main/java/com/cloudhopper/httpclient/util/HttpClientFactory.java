2c327e3df8237848d399bf82cdb31969491913ab
garth <garth@twitter.com>
2013-11-14 17:38:12 -0800

    file renaming prep for oss

--- src/main/java/com/cloudhopper/httpclient/util/HttpClientFactory.java
+++ src/main/java/com/cloudhopper/httpclient/util/HttpClientFactory.java
@@ -1,17 +1,3 @@
-/**
- * Copyright (C) 2011 Twitter, Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this
- * file except in compliance with the License. You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software distributed
- * under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
- * CONDITIONS OF ANY KIND, either express or implied. See the License for the
- * specific language governing permissions and limitations under the License.
- */
-
 package com.cloudhopper.httpclient.util;
 
 /*
@@ -37,11 +23,11 @@ package com.cloudhopper.httpclient.util;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.security.cert.CertificateException;
 import java.security.KeyManagementException;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
 import java.security.UnrecoverableKeyException;
+import java.security.cert.CertificateException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.conn.scheme.Scheme;
 import org.apache.http.impl.client.DefaultHttpClient;
