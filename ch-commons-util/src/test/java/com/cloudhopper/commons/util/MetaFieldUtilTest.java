16db07a1513bdccee72fa165e8409ae91bae9d83
Yihen Liu <yichen28@illinois.edu>
2022-11-24 21:36:18 -0600

    fixed flaky test

--- ch-commons-util/src/test/java/com/cloudhopper/commons/util/MetaFieldUtilTest.java
+++ ch-commons-util/src/test/java/com/cloudhopper/commons/util/MetaFieldUtilTest.java
@@ -45,14 +45,14 @@ public class MetaFieldUtilTest {
         Administrator emp = new Administrator();
         MetaFieldInfo[] fields = MetaFieldUtil.toMetaFieldInfoArray(emp);
         Assert.assertEquals(12, fields.length);
-        Assert.assertEquals("First Name", fields[0].name);
-        Assert.assertEquals("Last Name", fields[1].name);
-        Assert.assertEquals("email", fields[2].name);
-        Assert.assertEquals("ID", fields[3].name);
-        Assert.assertEquals("Active?", fields[4].name);
+        Assert.assertEquals("email", fields[0].name);
+        Assert.assertEquals("First Name", fields[1].name);
+        Assert.assertEquals("ID", fields[2].name);
+        Assert.assertEquals("Active?", fields[3].name);
+        Assert.assertEquals("Last Name", fields[4].name);
         Assert.assertEquals("loginCounter", fields[5].name);
-        Assert.assertEquals("The first name of this user", fields[0].description);
-        Assert.assertEquals("", fields[1].description);
+        Assert.assertEquals("The first name of this user", fields[1].description);
+        Assert.assertEquals("", fields[0].description);
         Assert.assertEquals("", fields[2].description);
         // set some values
         emp.setFirstName("John");
@@ -61,9 +61,9 @@ public class MetaFieldUtilTest {
         //for (MetaFieldInfo field : fields) {
         //    logger.debug("field name=" + field.name + ", value=" + field.value + ", description=" + field.description);
         //}
-        Assert.assertEquals("John", fields[0].value);
-        Assert.assertEquals("Doe", fields[1].value);
-        Assert.assertEquals("EMPTY", fields[2].value);
+        Assert.assertEquals("EMPTY", fields[0].value);
+        Assert.assertEquals("John", fields[1].value);
+        Assert.assertEquals("Doe", fields[4].value);
     }
 
 
